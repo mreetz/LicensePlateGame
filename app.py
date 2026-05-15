8a0dbb94d9d9c8a17d19cfcc4ad18c06cbca4eb9
Michael W. Reetz <mreetz@gmail.com>
2026-05-15 14:57:13 -0600

    Initialize database migrations

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     is_approved = db.Column(db.Boolean, default=False)
     is_admin = db.Column(db.Boolean, default=False)
     reset_token = db.Column(db.String(100), nullable=True)  # Added for password reset token
---
+++ migrations/versions/c339aa803588_initial_schema_baseline.py
@@ -0,0 +1,24 @@
+"""Initial schema baseline
+
+Revision ID: c339aa803588
+Revises: 
+Create Date: 2026-05-15 14:56:08.979674
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c339aa803588'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
